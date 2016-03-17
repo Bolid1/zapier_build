572a38859db6354ffe3d47193414e2ae86e7e2f4
vvargin <VDVUGaD@gmail.com>
2016-03-17 18:10:29 +0300

    New tests

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ var
   ZapBuilder = require(current_dir + '/zap_builder'),
   libs_dir = current_dir + '/libs',
   tests_dir = current_dir + '/tests',
+  new_tests_dir = current_dir + '/../tests',
   out = [],
   out_tests = [],
   out_folder = '/../result',
@@ -26,3 +27,49 @@ _.each(file_system.readdirSync(tests_dir), function (lib_name) {
 
 out_tests = [out_tests[0]].concat(out, out_tests.slice(1)).join("\n\n");
 eval(out_tests);
+
+console.log('===== new tests =====');
+_.each(['company', 'contact', 'lead', 'note', 'task'], function (entity) {
+  _.each(['action', 'hook'], function (type) {
+    var test_dir = [new_tests_dir, entity, type].join('/');
+    console.log(test_dir);
+    if (!file_system.existsSync(test_dir)) {
+      return;
+    }
+
+    _.each(file_system.readdirSync(test_dir), function (file_name) {
+      var
+        test_data = JSON.parse(file_system.readFileSync([test_dir, file_name].join('/'), 'utf8')),
+        action_name,
+        test_result,
+        result = '';
+
+      if (type === 'hook') {
+        action_name = [file_name.replace('.json', ''), entity, 'catch_hook'].join('_');
+      }
+
+      if (!(action_name && Zap[action_name])) {
+        return;
+      }
+
+      result += 'Zap.' + action_name;
+      try {
+        test_result = Zap[action_name](test_data.bundle);
+      } catch (ex) {
+        console.error(result + ' error!');
+        return;
+      }
+
+      result += ' worked... ';
+
+      if (hasDifference(test_data.result, test_result)) {
+        result += ' failed!';
+        console.error(result);
+      } else {
+        result += ' success!';
+        console.log(result);
+      }
+
+    });
+  });
+});
--- app/tests/1-header.js
+++ app/tests/1-header.js
@@ -6,3 +6,41 @@ var
   result;
 
 $.param = require("jquery-param");
+
+function hasDifference(obj_1, obj_2) {
+  var
+    keys_1 = _.keys(obj_1),
+    keys_2 = _.keys(obj_2),
+    has_difference;
+
+  if (_.difference(keys_1, keys_2).length) {
+    return true;
+  }
+
+
+  has_difference = false;
+  _.each(keys_1, function (key) {
+    if (has_difference) {
+      return;
+    }
+
+    has_difference = typeof obj_1[key] !== typeof obj_2[key];
+    if (has_difference) {
+      return;
+    }
+
+    if (_.isArray(obj_1[key]) || _.isObject(obj_1[key])) {
+      has_difference = hasDifference(obj_1[key], obj_2[key]);
+      return;
+    }
+
+    if (key === 'date_create' || key === 'last_modified') {
+      // @TODO: Fix this
+      return;
+    }
+
+    has_difference = obj_1[key] !== obj_2[key];
+  });
+
+  return has_difference;
+}
